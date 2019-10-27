fba95c5cb55d5dd2de2f2795429f438e1e3a460f
Arthur Nakano <arthurnakano@Leticias-MacBook-Air.local>
2019-10-27 14:06:34 -0300

    deploy test

---
+++ build/precache-manifest.07d22a80f933767b62f219804be056ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b2ad95b6c23ecc8d99d9f00011b903",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "c81344aa14b2da27af9a",
+    "url": "/build/static/css/main.7952db17.chunk.css"
+  },
+  {
+    "revision": "c65d3bce3c06bc98db7a",
+    "url": "/build/static/js/2.d2b715a8.chunk.js"
+  },
+  {
+    "revision": "c81344aa14b2da27af9a",
+    "url": "/build/static/js/main.44cbc05c.chunk.js"
+  },
+  {
+    "revision": "3351bb3a1182374a033e",
+    "url": "/build/static/js/runtime-main.f96ea02f.js"
+  },
+  {
+    "revision": "c5b01b24488621f886d64db77dd676a2",
+    "url": "/build/static/media/CalibreRegular.c5b01b24.woff"
+  },
+  {
+    "revision": "2b9ea546bad07e06302065917f2af008",
+    "url": "/build/static/media/calibri-bold-2.2b9ea546.ttf"
+  }
+]);
