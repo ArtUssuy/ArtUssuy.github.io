b36be1df1f15e77515012ee5968828028ab89c81
Arthur Nakano <arthurnakano@Leticias-MacBook-Air.local>
2019-11-02 13:53:02 -0300

    fixing index

---
+++ build/precache-manifest.1a19db51029b4015e284731d4e667410.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "554a4c9ee9487b2db787b85db244a5db",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "9f85eba5be346f3b3995",
+    "url": "/build/static/css/main.2e1cde98.chunk.css"
+  },
+  {
+    "revision": "07b753eb1625767c05ad",
+    "url": "/build/static/js/2.5d1e30b1.chunk.js"
+  },
+  {
+    "revision": "9f85eba5be346f3b3995",
+    "url": "/build/static/js/main.973aec2f.chunk.js"
+  },
+  {
+    "revision": "3351bb3a1182374a033e",
+    "url": "/build/static/js/runtime-main.f96ea02f.js"
+  },
+  {
+    "revision": "c5b01b24488621f886d64db77dd676a2",
+    "url": "/build/static/media/CalibreRegular.c5b01b24.woff"
+  },
+  {
+    "revision": "88905342188fcd71e312bdcebef295fc",
+    "url": "/build/static/media/CalibreRegularItalic.88905342.woff"
+  },
+  {
+    "revision": "2b9ea546bad07e06302065917f2af008",
+    "url": "/build/static/media/calibri-bold-2.2b9ea546.ttf"
+  }
+]);
